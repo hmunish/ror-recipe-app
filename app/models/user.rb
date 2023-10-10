392c12538665800b238a4de72c8b1834e26da0ed
Munish <hmunish5@gmail.com>
2023-10-10 12:15:08 +0200

    Add all the required views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :confirmable, :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     validates :password, presence: true
   validates :email, presence: true
 
