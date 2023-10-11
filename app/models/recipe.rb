af6fe5e425100764ce03961bf3bea393fb90d402
BenM4k <bennymakomo@gmail.com>
2023-10-11 11:23:19 +0200

    Fix public recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,14 +1,14 @@
 class Recipe < ApplicationRecord
-    belongs_to :user, foreign_key: :user_id
-    has_many :recipe_food
+  belongs_to :user, foreign_key: :user_id
+  has_many :recipe_food
 
-    validates :name, presence: true
-    validates :preparation_time, presence: true
-    validates :cooking_time, presence: true
-    validates :description, presence: true
+  validates :name, presence: true
+  validates :preparation_time, presence: true
+  validates :cooking_time, presence: true
+  validates :description, presence: true
 
-    # Sum prices
-    def self.total_price
-      sum(:price)
-    end
+  # Sum prices
+  def self.total_price
+    sum(:price)
   end
+end
